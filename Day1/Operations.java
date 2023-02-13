d1a1486447c4ad82328b02a7085aaecba421c8fe
Imad <imadsab33ayu@gmail.com>
2023-02-13 12:09:49 +0000

    day 1 practiece

--- Day1/Operations.java
+++ Day1/Operations.java
@@ -1,16 +1,24 @@
-class Operations{
-    public static void main(String[] args){
-        int a=54,b=45;
-        //Sum of two Numbers
-        System.out.println("Sum is :"+ (a+b));
+import java.util.Scanner;
 
-         //Substraction of two Numbers
-        System.out.println("Substraction is :"+ (a-b));
+class Operations {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-         //Multiplication of two Numbers
-        System.out.println("Multiplication is :"+ (a*b));
+        int a, b;
+        System.out.println("Enter Two values");
+        a = s.nextInt();
+        b=s.nextInt();
 
-         //Division of two Numbers
-        System.out.println("Division is :"+ (a/b));
+       // Sum of two Numbers
+        System.out.println("Sum is :" + (a + b));
+
+        // Substraction of two Numbers
+        System.out.println("Substraction is :" + (a - b));
+
+        // Multiplication of two Numbers
+        System.out.println("Multiplication is :" + (a * b));
+
+        // Division of two Numbers
+        System.out.println("Division is :" + (a / b));
     }
 }
