ff648d8aa3dc1b9bb120ae607d1baa9bc3150d50
Imad <imadsab33ayu@gmail.com>
2023-02-13 08:33:16 +0000

    Sample programs

---
+++ Day1/Operations.java
@@ -0,0 +1,16 @@
+class Operations{
+    public static void main(String[] args){
+        int a=54,b=45;
+        //Sum of two Numbers
+        System.out.println("Sum is :"+ (a+b));
+
+         //Substraction of two Numbers
+        System.out.println("Substraction is :"+ (a-b));
+
+         //Multiplication of two Numbers
+        System.out.println("Multiplication is :"+ (a*b));
+
+         //Division of two Numbers
+        System.out.println("Division is :"+ (a/b));
+    }
+}
